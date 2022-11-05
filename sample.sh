0724cfb19809ea70e3c735fa7bdc85fce4db3e80
shashidharkarre <shashidharkarre26@gmail.com>
2022-11-05 14:59:48 +0530

    added the changes

--- sample.sh
+++ sample.sh
@@ -5,3 +5,12 @@ echo -e "\e[35m I am printing yellow colour \e[0m"git
 echo -e "\e[36m I am printing yellow colour \e[0m"git 
 echo -e "\e[31m I am printing yellow colour \e[0m"git 
 echo -e "\e[32m I am printing yellow colour \e[0m"git 
+
+
+
+echo -e "\e[41m I am printing yellow colour \e[0m"git
+echo -e "\e[42m I am printing yellow colour \e[0m"git 
+echo -e "\e[43m I am printing yellow colour \e[0m"git 
+echo -e "\e[44m I am printing yellow colour \e[0m"git 
+echo -e "\e[45m I am printing yellow colour \e[0m"git 
+echo -e "\e[46m I am printing yellow colour \e[0m"git 
